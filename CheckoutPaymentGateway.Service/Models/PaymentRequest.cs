e4a331dd9b71c7a1ccdb004e6a905870e65ce006
Ehimah Obuse <ehimahobuse@gmail.com>
2022-04-19 08:08:12 +0100

    chore: use proper parameters in tests

--- CheckoutPaymentGateway.Service/Models/PaymentRequest.cs
+++ CheckoutPaymentGateway.Service/Models/PaymentRequest.cs
@@ -3,9 +3,12 @@ namespace CheckoutPaymentGateway.Service.Models
 {
 	public class PaymentRequest
 	{
-		public PaymentRequest()
-		{
-		}
-	}
+		public string CardHolderFullName { get; set; }
+		public string CardNumber { get; set; }
+		public string CardExpiryDate { get; set; }
+		public string CardCVV { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+    }
 }
 
--- CheckoutPaymentGateway.Tests.API/PaymentGatewayTests.cs
+++ CheckoutPaymentGateway.Tests.API/PaymentGatewayTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using CheckoutPaymentGateway.Service.Models;
 using CheckOutPaymentGateway.API.Controllers;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
@@ -24,7 +26,15 @@ public class PaymentGatewayTests: IClassFixture<WebApplicationFactory<CheckOutPa
         // Arrange
         var request = new HttpRequestMessage(HttpMethod.Post, REST_API_URL);
 
-        var payload = new { id = "value" };
+        var payload = new PaymentRequest
+        {
+            CardNumber = "1234123412341234",
+            CardHolderFullName = "EHIMAH OBUSE",
+            CardExpiryDate = "04/25",
+            CardCVV = "NGN",
+            Amount = 12.34,
+            Currency = "GBP",
+        };
 
         request.Content = new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json");
 
@@ -40,7 +50,8 @@ public class PaymentGatewayTests: IClassFixture<WebApplicationFactory<CheckOutPa
     public async void GetPayment_WhenCalledWithValidId_ReturnsPaymentItem()
     {
         // Act
-        var response = await httpClient.GetAsync(REST_API_URL);
+        var paymentId = Guid.NewGuid();
+        var response = await httpClient.GetAsync($"{REST_API_URL}/{paymentId}");
 
         // Assert
         response.EnsureSuccessStatusCode();
