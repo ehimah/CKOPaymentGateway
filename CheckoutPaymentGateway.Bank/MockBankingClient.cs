d5d3c8df73ff26fc97ba75cb749504460bca5273
Ehimah Obuse <ehimahobuse@gmail.com>
2022-04-19 09:55:46 +0100

    chore: implement banking client to process transaction

--- CheckoutPaymentGateway.Bank/MockBankingClient.cs
+++ CheckoutPaymentGateway.Bank/MockBankingClient.cs
@@ -10,9 +10,31 @@ namespace CheckoutPaymentGateway.Bank
 		{
 		}
 
-        Task<PaymentResponse> IBankingClient.ProcessPayment(PaymentRequest paymentRequest)
+        public Task<PaymentResponse> ProcessPayment(PaymentRequest paymentRequest)
         {
-            throw new NotImplementedException();
+            // build response
+            var response = new PaymentResponse() { ExternalReference = Guid.NewGuid() };
+
+            // validate payment request
+            if (!this.PaymentRequestIsValid(paymentRequest))
+            {
+                // decline the request
+                // TODO: decline with humane error message
+                response.Status = TransactionStatus.Declined;
+            }
+
+            // return response
+            return Task.FromResult(response);
+        }
+
+        private bool PaymentRequestIsValid(PaymentRequest paymentRequest)
+        {
+            // TODO: validate the following
+            // card length
+            // cvv length
+            // card holder name present
+            // card expiry is in future
+            return false;
         }
     }
 }
