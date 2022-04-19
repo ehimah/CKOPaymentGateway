645a40f55ed3f7106ad9a2780178012e0a5ed27f
Ehimah Obuse <ehimahobuse@gmail.com>
2022-04-19 21:36:45 +0100

    chore: added swagger and auth

--- CheckOutPaymentGateway.API/Startup.cs
+++ CheckOutPaymentGateway.API/Startup.cs
@@ -15,6 +15,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
 
 namespace CheckOutPaymentGateway.API
 {
@@ -39,15 +41,27 @@ namespace CheckOutPaymentGateway.API
             services.AddScoped<IBankingClient, MockBankClient>();
             services.AddSingleton<IPaymentRepository, InMemoryPaymentRepository>();
 
-            //// 1. Add Authentication Services
-            //services.AddAuthentication(options =>
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CKO PaymentGateway API", Version = "v1" });
+            });
+
+            string authority = Configuration["Auth0:Authority"];
+            // 1. Add Authentication Services
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["Auth0:Authority"];
+                options.Audience = Configuration["Auth0:Audience"];
+            });
+
+            //services.AddAuthorization(options =>
             //{
-            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //}).AddJwtBearer(options =>
-            //{
-            //    options.Authority = Configuration["Auth0:Authority"];
-            //    options.Audience = Configuration["Auth0:Audience"];
+            //    options.AddPolicy("read-write:test-scope",
+            //        policy => policy.Requirements.Add(new ScopeRequirement("read-write:test-scope", authority)));
             //});
         }
 
@@ -59,11 +73,23 @@ namespace CheckOutPaymentGateway.API
                 app.UseDeveloperExceptionPage();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CKO PaymentGateway API V1");
+                // set swagger UI url to be app route
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
